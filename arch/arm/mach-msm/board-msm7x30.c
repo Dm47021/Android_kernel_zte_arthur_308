4969b5e6ba104378a7c76dbfd6042214f9f6af49
Dm47021 <dm47021@hotmail.com>
2013-11-14 00:43:57 -0500

    more memory

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -101,19 +101,17 @@ when          who    what, where, why                      	comment tag
 #include <linux/proc_fs.h>
 void msm7x30_ts_init(void); 
 
-
-//#define MSM_PMEM_SF_SIZE	0x1F00000
-#define MSM_PMEM_SF_SIZE	(40*1024*1024)
-
+/* Propper Memory Setup, Not that ZTE ZBS */
+#define MSM_PMEM_SF_SIZE	        0x1700000
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
-#define MSM_FB_SIZE            0x780000
+#define MSM_FB_SIZE                     0x780000
 #else
-#define MSM_FB_SIZE            0x500000
+#define MSM_FB_SIZE                     0x500000
 #endif
-#define MSM_PMEM_ADSP_SIZE      0x0 // 0x3200000//PMEM ->5M
-#define MSM_FLUID_PMEM_ADSP_SIZE	0x0 // 0x2800000
-#define PMEM_KERNEL_EBI0_SIZE   0x600000
-#define MSM_PMEM_AUDIO_SIZE     0x0 // 0x200000
+#define MSM_PMEM_ADSP_SIZE              0x1800000
+#define MSM_FLUID_PMEM_ADSP_SIZE	0x2800000
+#define PMEM_KERNEL_EBI0_SIZE           0x600000
+#define MSM_PMEM_AUDIO_SIZE             0x200000   
 
 #define MSM_FB_OVERLAY0_WRITEBACK_SIZE roundup((544 * 960 * 3 * 2), 4096)
 #define PMIC_GPIO_INT		27
@@ -6043,7 +6041,15 @@ static void __init zte_fixup(struct machine_desc *desc, struct tag *tags,
 				  char **cmdline, struct meminfo *mi)
 {
 	g_zte_ftm_flag_fixup = parse_tag_zteftm((const struct tag *)tags);
-}
+        for (; tags->hdr.size; tags = tag_next(tags)) {
+     if (tags->hdr.tag == ATAG_MEM && tags->u.mem.start ==
+               DDR1_BANK_BASE) {
+         ebi1_phys_offset = DDR1_BANK_BASE;
+         phys_add = DDR1_BANK_BASE;
+         break;
+     }
+   }
+ }
 
 int get_ftm_from_tag(void)
 {
